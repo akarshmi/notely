e2b3431f42b4a1585d9a7aa49628afc04791fa3f
Akarsh Mishra <akarshmi.am@gmail.com>
2025-07-27 15:34:54 +0530

    not initial for production

--- backend/notely/app.py
+++ backend/notely/app.py
@@ -271,5 +271,5 @@ def get_note_stats():
         'recentlyEdited': recently_edited
     }), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+    # app.run(debug=False)
