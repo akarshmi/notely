48fe56636f8efad7eb029305626b758395dcc370
Akarsh Mishra <akarshmi.am@gmail.com>
2025-07-27 17:09:04 +0530

    Added IMG1.png for Hero section

--- frontend/notely/src/components/Hero.jsx
+++ frontend/notely/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Hero.css";
 
-import Img1 from '../assets/IMG1.png'
+import Img1 from '../assets/IMG1.png';
 import Button from "./Button";
 import { Link } from "react-router-dom";
 
